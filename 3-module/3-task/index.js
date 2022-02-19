34b031a00657b95263b35516caa6b16f34c9079e
vadim <vadim543@bk.ru>
2022-02-19 18:01:14 +0800

    Add 3-module/3-task fix

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,5 +2,5 @@ function camelize(str) {
   return str
         .split('-')
         .map((str, index) => index > 0 ? str[0].toUpperCase() + str.slice(1) : str)
-        .join();
+        .join('');
 }
